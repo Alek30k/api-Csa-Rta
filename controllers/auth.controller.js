bb12f0ae8d449e413b49f060e961d26360f565bf
Alek30k <alemank23k@gmail.com>
2023-06-02 23:28:32 -0300

    last commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,43 +1,118 @@
-import User from "../models/user.model.js";
+// import User from "../models/user.model.js";
 
-export const register = async (req, res) => {
+// export const register = async (req, res) => {
+//   try {
+//     const newUser = new User(req.body);
+//     await newUser.save();
+//     res.status(201).send("User has been created.");
+//   } catch (error) {
+//     res.status(500).send("Something went wrong!");
+//   }
+// };
+
+// export const login = async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({ email: req.body.email });
+
+//     if (!user) res.status(404).send("User not found!");
+
+//     // const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+//     // if (!isCorrect)
+//     //   return next(createError(400, "Wrong password or username!"));
+
+//     const { ...info } = user._doc;
+//     res
+//       .cookie("accessToken", {
+//         httpOnly: true,
+//       })
+//       .status(200)
+//       .send(info);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// export const logout = async (req, res) => {
+//   res
+//     .clearCookie("accessToken", {
+//       secure: true,
+//       sameSite: "none",
+//     })
+//     .status(200)
+//     .send("User has been logged out.");
+// };
+
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
   try {
-    const newUser = new User(req.body);
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
     await newUser.save();
-    res.status(201).send("User has been created.");
-  } catch (error) {
-    res.status(500).send("Something went wrong!");
+    res.status(200).send("User has been created!");
+  } catch (err) {
+    next(err);
   }
 };
 
-export const login = async (req, res, next) => {
+export const signin = async (req, res, next) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "User not found!"));
 
-    if (!user) res.status(404).send("User not found!");
+    const isCorrect = bcrypt.compare(req.body.password, user.password);
 
-    // const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    // if (!isCorrect)
-    //   return next(createError(400, "Wrong password or username!"));
+    if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
 
-    const { ...info } = user._doc;
+    const token = jwt.sign({ id: user._id }, "agsjdg7657HGDAJ67rjdhgadhkjf");
+    const { password, ...others } = user._doc;
+
     res
-      .cookie("accessToken", {
+      .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .send(info);
+      .json(others);
   } catch (err) {
-    console.log(err);
+    next(err);
   }
 };
 
-export const logout = async (req, res) => {
-  res
-    .clearCookie("accessToken", {
-      secure: true,
-      sameSite: "none",
-    })
-    .status(200)
-    .send("User has been logged out.");
+export const googleAuth = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, "agsjdg7657HGDAJ67rjdhgadhkjf");
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(user._doc);
+    } else {
+      const newUser = new User({
+        ...req.body,
+        fromGoogle: true,
+      });
+      const savedUser = await newUser.save();
+      const token = jwt.sign(
+        { id: savedUser._id },
+        "agsjdg7657HGDAJ67rjdhgadhkjf"
+      );
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .json(savedUser._doc);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,26 @@
+// import express from "express";
+// import { register, login, logout } from "../controllers/auth.controller.js";
+
+// const router = express.Router();
+
+// router.post("/register", register);
+// router.post("/login", login);
+// router.post("/logout", logout);
+
+// export default router;
+
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { googleAuth, signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
+//CREATE A USER
+router.post("/signup", signup);
+
+//SIGN IN
+router.post("/signin", signin);
+
+//GOOGLE AUTH
+router.post("/google", googleAuth);
 
 export default router;
