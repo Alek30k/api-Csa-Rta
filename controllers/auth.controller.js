34d1afdcb0948e77bb0ee6bb4f88905b8d4aaa03
Alek23k <alemank23k@gmail.com>
2023-06-14 20:48:48 -0300

    last commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -20,7 +20,7 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   try {
-    const user = await User.findOne({ username: req.body.email });
+    const user = await User.findOne({ email: req.body.email });
     if (!user) return next(createError(404, "User or email not found!"));
 
     const isCorrect = bcrypt.compare(req.body.password, user.password);
