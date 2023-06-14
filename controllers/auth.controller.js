87e5561cff53a7eb1ecdf92422049c0b37d00b8e
Alek23k <alemank23k@gmail.com>
2023-06-14 18:54:03 -0300

    last commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,47 +1,3 @@
-// import User from "../models/user.model.js";
-
-// export const register = async (req, res) => {
-//   try {
-//     const newUser = new User(req.body);
-//     await newUser.save();
-//     res.status(201).send("User has been created.");
-//   } catch (error) {
-//     res.status(500).send("Something went wrong!");
-//   }
-// };
-
-// export const login = async (req, res, next) => {
-//   try {
-//     const user = await User.findOne({ email: req.body.email });
-
-//     if (!user) res.status(404).send("User not found!");
-
-//     // const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-//     // if (!isCorrect)
-//     //   return next(createError(400, "Wrong password or username!"));
-
-//     const { ...info } = user._doc;
-//     res
-//       .cookie("accessToken", {
-//         httpOnly: true,
-//       })
-//       .status(200)
-//       .send(info);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// export const logout = async (req, res) => {
-//   res
-//     .clearCookie("accessToken", {
-//       secure: true,
-//       sameSite: "none",
-//     })
-//     .status(200)
-//     .send("User has been logged out.");
-// };
-
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -49,14 +5,26 @@ import { createError } from "../error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-  try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    const newUser = new User({ ...req.body, password: hash });
+  // try {
+  //   const salt = bcrypt.genSaltSync(10);
+  //   const hash = bcrypt.hashSync(req.body.password, salt);
+  //   const newUser = new User({ ...req.body, password: hash });
 
+  //   await newUser.save();
+  //   res.status(200).send("User has been created!");
+  // } catch (err) {
+  //   next(err);
+  // }
+
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
     await newUser.save();
-    res.status(200).send("User has been created!");
-  } catch (err) {
+    res.status(201).send("User has been created.");
+  } catch (error) {
     next(err);
   }
 };
